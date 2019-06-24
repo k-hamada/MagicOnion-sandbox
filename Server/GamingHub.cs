dd0b928ceea7d255a9c8864e99b022fb71637934
k-hamada <k.hamada.1988@gmail.com>
2019-06-24 15:39:02 +0900

    write Server

---
+++ Server/GamingHub.cs
@@ -0,0 +1,38 @@
+using System.Threading.Tasks;
+using System.Linq;
+using MagicOnion.Server.Hubs;
+using UnityEngine;
+
+namespace Server
+{
+    public class GamingHub : StreamingHubBase<IGamingHub, IGamingHubReceiver>, IGamingHub
+    {
+        IGroup room;
+        Player self;
+        IInMemoryStorage<Player> storage;
+
+        public async Task<Player[]> JoinAsync(string roomName, string userName, Vector3 position, Quaternion rotation)
+        {
+            self = new Player() { Name = userName, Position = position, Rotation = rotation };
+
+            (room, storage) = await Group.AddAsync(roomName, self);
+
+            Broadcast(room).OnJoin(self);
+
+            return storage.AllValues.ToArray();
+        }
+
+        public async Task LeaveAsync()
+        {
+            await room.RemoveAsync(this.Context);
+            Broadcast(room).OnLeave(self);
+        }
+
+        public async Task MoveAsync(Vector3 position, Quaternion rotation)
+        {
+            self.Position = position;
+            self.Rotation = rotation;
+            Broadcast(room).OnMove(self);
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+using MagicOnion.Hosting;
+using MagicOnion.Server;
+using Grpc.Core;
 
 namespace Server
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GrpcEnvironment.SetLogger(new Grpc.Core.Logging.ConsoleLogger());
+
+            await MagicOnionHost.CreateDefaultBuilder()
+                .UseMagicOnion(
+                    new MagicOnionOptions(isReturnExceptionStackTraceInErrorDetail: true),
+                    new ServerPort("localhost", 12345, ServerCredentials.Insecure))
+                .RunConsoleAsync();
         }
     }
 }
